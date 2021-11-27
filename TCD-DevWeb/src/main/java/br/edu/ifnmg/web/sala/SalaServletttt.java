fd3600cd3d7a761e87f85bd6d6b7a54c2531dd1b
Filip3ra <filipimacielrj@gmail.com>
2021-11-27 10:16:26 -0300

    Navegação simples realizada, cadastro usuário OK

--- TCD-DevWeb/src/main/java/br/edu/ifnmg/web/sala/SalaServletttt.java
+++ TCD-DevWeb/src/main/java/br/edu/ifnmg/web/sala/SalaServletttt.java
@@ -1,14 +1,12 @@
 /*
- * Este código foi desenvolvido com intuito de aprender sobre desenvolvimento de aplicações WEB.
- * Este trabalho foi desenvolvido na disciplina de Desenvolvimento Web do IFNMG - Campus - Montes Claros.
- * Este é um projeto de código aberto que pode ser utilizado para ajudar as pessoas no desenvolvimento de aplicações Web.
- * Copyright (C) 2021 Lucas <lsm3 at aluno.ifnmg.edu.br>
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package br.edu.ifnmg.web.sala;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,14 +15,18 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Lucas <lsm3 at aluno.ifnmg.edu.br>
+ * @author Filipjkhghjgfhjfhjfhj7
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
-@WebServlet(name = "salaServlet", urlPatterns = {"/sala"})
-public class salaServlet extends HttpServlet {
-    
-    @Inject
-    private SalaBeanLocal salaBean;
-    
+@WebServlet(name = "SalaServlet", urlPatterns = {"/SalaServlet"})
+public class SalaServlettttSalaServletttt extends HttpServlet {
+
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -36,42 +38,16 @@ public class salaServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        Sala sala = new Sala(request.getParameter("nome"), Integer.parseInt(request.getParameter("capacidade")));
-        
-        salaBean.salvar(sala);
-        
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet salaServlet</title>");            
+            out.println("<title>Servlet SalaServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            
-            out.println("<h1>Cadastro de filme.</h1>");
-            
-            out.println("<table>");
-            out.println("<thead>");
-            out.println("<tr>");
-            out.println("<th>Nome</th>");
-            out.println("<th>Capacidade</th>");
-            out.println("<th>Tipo da Tela</th>");
-            out.println("</tr>");
-            out.println("</thead>");
-            out.println("<tbody>");
-            out.println("<tr>");
-            out.println("<td>" + sala.getNome() + "</td>");
-            out.println("<td>" + sala.getCapacidade() + "</td>");
-            out.println("</tr>");
-            out.println("</tbody>");
-            out.println("</table>");
-            out.println("<p>Sala criada com sucesso!</p>");
-            //out.println("<button><a href=\"http://localhost:8080/SJakartaServer-1.0-SNAPSHOT/cadastrofilme.html\">Retornar</a></button>"); 
-            
-            //out.println("<h1>Servlet salaServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet SalaServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
